8bd0ce87b0a3187a1716880c6035e5350571af68
Shigenori Taya <shigenori.taya@gmail.com>
2014-01-04 00:18:50 +0900

    add rember Chap03 p41 L5

--- rember.scm
+++ rember.scm
@@ -2,9 +2,8 @@
   (lambda (a lat)
     (cond
       ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) a) (cdr lat))
-              (else (cons (car lat)
-                          (rember a (cdr lat)))))))))
+      ((eq? (car lat) a) (cdr lat))
+      (else (cons (car lat)
+                  (rember a (cdr lat)))))))
 
 (rember `and `(bacon lettuce and tomato))
