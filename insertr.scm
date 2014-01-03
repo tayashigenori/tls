8c778cdd1094e6bfb14385e2052a6a1e3b39488d
Shigenori Taya <shigenori.taya@gmail.com>
2014-01-04 00:31:10 +0900

    add insertR Chap03 p51 L1

--- insertr.scm
+++ insertr.scm
@@ -2,11 +2,12 @@
   (lambda (new old lat)
     (cond
       ((null? lat) (quote ()))
-      (else
-       (cond
-         ((eq? (car lat) old) (cdr lat))
-         (else (cons (car lat)
-                     (insertR new old
-                              (cdr lat)))))))))
+      (else (cond
+              ((eq? (car lat) old)
+               (cons new (cdr lat)))
+              (else (cons (car lat)
+                          (insertR new old
+                                   (cdr lat)))))))))
 
 (insertR `topping `fudge `(ice cream with fudge for dessert))
+; this returns unexpected result `(ice cream with topping for dessert)
