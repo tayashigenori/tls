086a4c4bdbd86a35b9b8eb46032ff83c77c73bdc
Shigenori Taya <shigenori.taya@gmail.com>
2014-01-04 00:17:41 +0900

    add rember Chap03 p38 L1

--- rember.scm
+++ rember.scm
@@ -4,7 +4,7 @@
       ((null? lat) (quote ()))
       (else (cond
               ((eq? (car lat) a) (cdr lat))
-              (else (rember a (cdr lat))))))))
+              (else (cons (car lat)
+                          (rember a (cdr lat)))))))))
 
 (rember `and `(bacon lettuce and tomato))
-; this returns unexpected result `(tomato)
