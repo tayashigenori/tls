449da8965c0b2aabde30b9d928e75ecdef8c3ea2
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-01 00:59:43 +0900

    modify rember Chap05 p97 L3

--- rember.scm
+++ rember.scm
@@ -4,10 +4,8 @@
   (lambda (s l)
     (cond
       ((null? l) (quote ()))
-      (else (cond
-              ((oequal? (car l) s) (cdr l))
-              (else (cons (car l)
-                          (rember s
-                                  (cdr l)))))))))
+      ((oequal? (car l) s) (cdr l))
+      (else (cons (car l)
+                  (rember s (cdr l)))))))
 
 (rember `sauce `(soy sauce and tomato sauce))
