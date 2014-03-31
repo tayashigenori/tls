3fe0b7d81d6cc1984a5e0275c947551dee09d417
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-01 00:52:57 +0900

    modify oequal? (equal?) Chap05 p95 L5

--- oequal.scm
+++ oequal.scm
@@ -7,8 +7,8 @@
     (cond
       ((and (atom? s1) (atom? s2))
        (eqan? s1 s2))
-      ((atom? s1) #f)
-      ((atom? s2) #f)
+      ((or (atom? s1) (atom? s2))
+       #f)
       (else (eqlist? s1 s2)))))
 
 
