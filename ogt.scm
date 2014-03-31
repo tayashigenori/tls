fb3c304631f9f817edc59504537be5bec96e3e00
Shigenori Taya <shigenori.taya@gmail.com>
2014-03-31 13:00:55 +0900

    modify > (ogt, o greater than) Chap04 p75 L7

--- ogt.scm
+++ ogt.scm
@@ -3,13 +3,9 @@
 (define ogt
   (lambda (n m)
     (cond
-      ((zero? m) #t)
       ((zero? n) #f)
+      ((zero? m) #t)
       (else (ogt (sub1 n) (sub1 m))))))
 
-; > (ogt 12 133)
-; #f
-; > (ogt 120 11)
-; #t
 ; > (ogt 3 3)
-; #t ;; wrong!!
+; #f
