c64b29cc717d96dcb269b4f7608857e3d5839206
Shigenori Taya <shigenori.taya@gmail.com>
2014-03-31 15:58:19 +0900

    modify rempick Chap04 p81 L3

--- rempick.scm
+++ rempick.scm
@@ -1,12 +1,13 @@
 (load "./common/sub1.scm")
+(load "./one.scm")
 
 (define rempick
   (lambda (n lat)
     (cond
-      ((zero? (sub1 n)) (cdr lat))
+      ((one? n) (cdr lat))
       (else (cons (car lat)
                   (rempick (sub1 n)
                            (cdr lat)))))))
 
-; > (rempick 3 `(hotdogs with hot mustard))
-; (hotdogs with mustard)
+; > (rempick 3 `(lemon meringue salty pie))
+; (lemon meringue pie)
