f65c1477d106b4f7b0c82d285c23d6837c4e0c69
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-01 00:55:14 +0900

    modify eqlist? Chap05 p96 L1

--- eqlist.scm
+++ eqlist.scm
@@ -1,20 +1,12 @@
-(load "./common/atom.scm")
-(load "./eqan.scm")
+(load "./oequal.scm")
 
 (define eqlist?
   (lambda (l1 l2)
     (cond
       ((and (null? l1) (null? l2)) #t)
       ((or (null? l1) (null? l2)) #f)
-      ((and (atom? (car l1))
-            (atom? (car l2)))
-       (and (eqan? (car l1) (car l2))
-            (eqlist? (cdr l1) (cdr l2))))
-      ((or (atom? (car l1))
-           (atom? (car l2)))
-       #f)
       (else
-       (and (eqlist? (car l1) (car l2))
+       (and (oequal? (car l1) (car l2))
             (eqlist? (cdr l1) (cdr l2)))))))
 
 
