85e08495a7ff0acf2cbbb53b8efdd3c3d5568b64
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-01 01:36:40 +0900

    modify value Chap06 p108 L1

--- value.scm
+++ value.scm
@@ -2,21 +2,24 @@
 (load "./o+.scm")
 (load "./omultiply.scm")
 (load "./oexpt.scm")
+(load "./1st-sub-exp.scm")
+(load "./2nd-sub-exp.scm")
+(load "./operator.scm")
 
 (define value
   (lambda (nexp)
     (cond
       ((atom? nexp) nexp)
-      ((eq? (car nexp) (quote o+))
-       (o+ (value (cdr nexp))
-           (value (cdr (cdr nexp)))))
-      ((eq? (car nexp) (quote omultiply))
-       (omultiply (value (cdr nexp))
-                  (value (cdr (cdr nexp)))))
+      ((eq? (operator nexp) (quote o+))
+       (o+ (value (1st-sub-exp nexp))
+           (value (2nd-sub-exp nexp))))
+      ((eq? (operator nexp) (quote omultiply))
+       (omultiply (value (1st-sub-exp nexp))
+                  (value (2nd-sub-exp nexp))))
       (else
-       (oexpt (value (cdr nexp))
-              (value (cdr (cdr nexp))))))))
+       (oexpt (value (1st-sub-exp nexp))
+              (value (2nd-sub-exp nexp)))))))
 
 
 ; > (value `(o+ 1 3))
-; returns error "mcar: expects argument of type <mutable-pair>; given ()"
+; 4
