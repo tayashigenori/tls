a91c57e872ad8b99d335b4e2f4314c068fef8ae0
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-02 01:30:19 +0900

    modify intersect? Chap07 p117 L2

--- intersect.scm
+++ intersect.scm
@@ -4,11 +4,9 @@
   (lambda (set1 set2)
     (cond
       ((null? set1) #f)
-      (else
-       (cond
-         ((member? (car set1) set2) #t)
-         (else (intersect?
-                (cdr set1) set2)))))))
+      ((member? (car set1) set2) #t)
+      (else (intersect?
+             (cdr set1) set2)))))
 
 
 ; > (intersect? `(stewed tomatoes and macaroni) `(macaroni and cheese))
