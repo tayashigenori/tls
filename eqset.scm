6c3473035ddb88d7686bc490201d1289f56613f6
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-02 01:27:21 +0900

    modify eqset? Chap07 p116 L4

--- eqset.scm
+++ eqset.scm
@@ -3,9 +3,8 @@
 (define eqset?
   (lambda (set1 set2)
     (cond
-      ((subset? set1 set2)
-       (subset? set2 set1))
-      (else #f))))
+      (else (and (subset? set1 set2)
+                 (subset? set2 set1))))))
 
 
 ; > (eqset? `(6 large chickens with wings) `(6 chickens with large wings))
