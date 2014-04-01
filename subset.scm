c2882f2a426647dc2b2ecec6084088f4980f4250
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-02 01:25:16 +0900

    modify subset? Chap07 p116 L1

--- subset.scm
+++ subset.scm
@@ -4,9 +4,9 @@
   (lambda (set1 set2)
     (cond
       ((null? set1) #t)
-      ((member? (car set1) set2)
-       (subset? (cdr set1) set2))
-      (else #f))))
+      (else
+       (and (member? (car set1) set2)
+       (subset? (cdr set1) set2))))))
 
 
 ; > (subset? `(5 chicken wings) `(5 hamburgers 2 pieces fried chicken and light duckling wings))
