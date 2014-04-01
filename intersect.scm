a58552aa65a8007581e0fbcf00ea29da2af943df
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-02 01:32:37 +0900

    add intersect Chap07 p118 L1

--- intersect.scm
+++ intersect.scm
@@ -8,6 +8,17 @@
                 (intersect?
                  (cdr set1) set2))))))
 
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote ()))
+      ((member? (car set1) set2)
+       (cons (car set1)
+             (intersect (cdr set1) set2)))
+      (else (intersect (cdr set1) set2)))))
 
 ; > (intersect? `(stewed tomatoes and macaroni) `(macaroni and cheese))
 ; #t
+
+; > (intersect `(stewed tomatoes and macaroni) `(macaroni and cheese))
+; (and macaroni)
