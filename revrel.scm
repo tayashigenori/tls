a102774b29c1c1bbb334db0ff5e7d40f2f4d0b68
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-03 01:06:54 +0900

    modify revrel Chap07 p123 L1

--- revrel.scm
+++ revrel.scm
@@ -1,14 +1,10 @@
-(load "./build.scm")
-(load "./first.scm")
-(load "./second.scm")
+(load "./revpair.scm")
 
 (define revrel
   (lambda (rel)
     (cond
       ((null? rel) (quote ()))
-      (else (cons (build
-                   (second (car rel))
-                   (first (car rel)))
+      (else (cons (revpair (car rel))
                   (revrel (cdr rel)))))))
 
 
