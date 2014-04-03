2a77af034a09d24556fdb0e1864e3e9f60b396c6
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-04 01:40:42 +0900

    modify rember-f Chap08 p130 L6

--- rember-f.scm
+++ rember-f.scm
@@ -1,18 +1,11 @@
 (load "./oequal.scm")
 
 (define rember-f
-  (lambda (test? a l)
-    (cond
-      ((null? l) (quote ()))
-      ((test? (car l) a) (cdr l))
-      (else (cons (car l)
-                  (rember-f test? a
-                            (cdr l)))))))
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+        ((null? l) (quote ()))
+        ((test? (car l) a) (cdr l))
+        (else (cons (car l) ...))))))
 
-
-; > (rember-f = 5 `(6 2 5 3))
-; (6 2 3)
-; > (rember-f eq? `jelly `(jelly beans are good))
-; (beans are good)
-; > (rember-f oequal? `(pop corn) `(lemonade (pop corn) and (cake)))
-; (lemonade and (cake))
+; stub
