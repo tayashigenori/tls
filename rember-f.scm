aa9d9a4e3f9e3ecd3c54495b4bbb5ae5d41c09c2
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-04 01:43:20 +0900

    modify rember-f Chap08 p131 L5

--- rember-f.scm
+++ rember-f.scm
@@ -6,6 +6,14 @@
       (cond
         ((null? l) (quote ()))
         ((test? (car l) a) (cdr l))
-        (else (cons (car l) ...))))))
+        (else (cons (car l)
+                    ((rember-f test?) a
+                                      (cdr l))))))))
 
-; stub
+
+; > ((rember-f eq?) `tuna `(tuna salad is good))
+; (salad is good)
+; > ((rember-f eq?) `tuna `(shrimp salad and tuna salad))
+; (shrimp salad and salad)
+; > ((rember-f eq?) `eq? `(oequal? eq? eqan? eqlist? eqpair?))
+; (oequal? eqan? eqlist? eqpair?)
