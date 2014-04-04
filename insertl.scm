81ca39dcff1b159320493b3ec6331055c390beda
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-05 01:04:17 +0900

    modify insertL Chap08 p134 L2

--- insertl.scm
+++ insertl.scm
@@ -1,12 +1,7 @@
-(define insertL
-  (lambda (new old lat)
-    (cond
-      ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) old)
-               (cons new lat))
-              (else (cons (car lat)
-                          (insertL new old
-                                   (cdr lat)))))))))
+(load "./insert-g.scm")
+(load "./seqL.scm")
 
-(insertL `topping `fudge `(ice cream with fudge for dessert))
+(define insertL (insert-g seqL))
+
+; > (insertL `topping `fudge `(ice cream with fudge for dessert))
+; (ice cream with topping fudge for dessert)
