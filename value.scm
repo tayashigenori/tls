eddd1c0820f0e3e77fb021b3b6d2d9993bad786c
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-05 01:33:13 +0900

    modify value Chap08 p137 L1

--- value.scm
+++ value.scm
@@ -1,26 +1,18 @@
 (load "./common/atom.scm")
-(load "./o+.scm")
-(load "./omultiply.scm")
-(load "./oexpt.scm")
 (load "./1st-sub-exp.scm")
 (load "./2nd-sub-exp.scm")
 (load "./operator.scm")
+(load "./atom-to-function.scm")
 
 (define value
   (lambda (nexp)
     (cond
       ((atom? nexp) nexp)
-      ((eq? (operator nexp)
-            (quote o+))
-       (o+ (value (1st-sub-exp nexp))
-           (value (2nd-sub-exp nexp))))
-      ((eq? (operator nexp)
-            (quote omultiply))
-       (omultiply (value (1st-sub-exp nexp))
-                  (value (2nd-sub-exp nexp))))
       (else
-       (oexpt (value (1st-sub-exp nexp))
-              (value (2nd-sub-exp nexp)))))))
+       ((atom-to-function
+         (operator nexp))
+        (value (1st-sub-exp nexp))
+        (value (2nd-sub-exp nexp)))))))
 
 
 ; > (value 13)
