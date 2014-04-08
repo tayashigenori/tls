4b0b853d386b1de1ff34cfb9fde0cc3d4e5fbe28
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-09 02:04:40 +0900

    revert olength Chap09 p173 L1

--- olength.scm
+++ olength.scm
@@ -4,86 +4,13 @@
   ((lambda (mk-length)
      (mk-length mk-length))
    (lambda (mk-length)
-     ((lambda (length)
-        (lambda (l)
-          (cond
-            ((null? l) 0)
-            (else (add1 (length (cdr l)))))))
-      (mk-length mk-length))))
+     (lambda (l)
+       (cond
+         ((null? l) 0)
+         (else (add1
+                ((mk-length mk-length)
+                 (cdr l))))))))
   )
 
-; doesn't work!!
-
-; to evaluate the following expression:
-;   ((lambda (mk-length)
-;      (mk-length mk-length))
-;    (lambda (mk-length)
-;      ((lambda (length)
-;         (lambda (l)
-;           (cond
-;             ((null? l) 0)
-;             (else (add1 (length (cdr l)))))))
-;       (mk-length mk-length))))
-
-; we need to evaluate the following expression:
-;   ((lambda (mk-length)
-;      ((lambda (length)
-;         (lambda (l)
-;           (cond
-;             ((null? l) 0)
-;             (else (add1 (length (cdr l)))))))
-;       (mk-length mk-length)))
-;    (lambda (mk-length)
-;      ((lambda (length)
-;         (lambda (l)
-;           (cond
-;             ((null? l) 0)
-;             (else (add1 (length (cdr l)))))))
-;       (mk-length mk-length))))
-
-; which requires us to evaluate the following expression:
-;   ((lambda (length)
-;      (lambda (l)
-;        (cond
-;          ((null? l) 0)
-;          (else (add1 (length (cdr l)))))))
-;   ((lambda (mk-length)
-;      ((lambda (length)
-;         (lambda (l)
-;           (cond
-;             ((null? l) 0)
-;             (else (add1 (length (cdr l)))))))
-;       (mk-length mk-length)))
-;    (lambda (mk-length)
-;      ((lambda (length)
-;         (lambda (l)
-;           (cond
-;             ((null? l) 0)
-;             (else (add1 (length (cdr l)))))))
-;       (mk-length mk-length))))
-
-; which requires us to evaluate the following expression:
-;   ((lambda (length)
-;      (lambda (l)
-;        (cond
-;          ((null? l) 0)
-;          (else (add1 (length (cdr l)))))))
-;   ((lambda (length)
-;      (lambda (l)
-;        (cond
-;          ((null? l) 0)
-;          (else (add1 (length (cdr l)))))))
-;   ((lambda (mk-length)
-;      ((lambda (length)
-;         (lambda (l)
-;           (cond
-;             ((null? l) 0)
-;             (else (add1 (length (cdr l)))))))
-;       (mk-length mk-length)))
-;    (lambda (mk-length)
-;      ((lambda (length)
-;         (lambda (l)
-;           (cond
-;             ((null? l) 0)
-;             (else (add1 (length (cdr l)))))))
-;       (mk-length mk-length))))
+; > (olength `(hotdogs with mustard sauerkraut and pickles))
+; 6
