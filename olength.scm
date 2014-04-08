7641261da42d917e2cda0729f0f546f2c765d713
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-09 01:17:24 +0900

    modify length Chap09 p162 L4

--- olength.scm
+++ olength.scm
@@ -1,10 +1,10 @@
 (load "./common/add1.scm")
 
 (define olength
-  (lambda (lat)
+  (lambda (l)
     (cond
-      ((null? lat) 0)
-      (else (add1 (olength (cdr lat)))))))
+      ((null? l) 0)
+      (else (add1 (length (cdr l)))))))
 
 
 ; > (olength `(hotdogs with mustard sauerkraut and pickles))
