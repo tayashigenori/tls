fe060532a53ddf302565b5b925a4a19a8751cc94
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-09 02:11:33 +0900

    modify Y Chap09 p175 L2

--- y.scm
+++ y.scm
@@ -1,9 +1,8 @@
-; see ccc2badbf20c919b0dca35853ca1ae2302391c6a
-(lambda (le)
-  ((lambda (mk-length)
-     (mk-length mk-length))
-   (lambda (mk-length)
-     (le (lambda (x)
-           ((mk-length mk-length) x))))))
+(define Y
+  (lambda (le)
+    ((lambda (f) (f f))
+     (lambda (f)
+       (le (lambda (x) ((f f) x)))))))
 
 
+; (Y Y) does not work!!
