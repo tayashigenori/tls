cfddca45471f47796c512a936b793b28bcd8ff7a
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-09 01:41:29 +0900

    modify olength<=1 Chap09 p163 L3

--- olength1.scm
+++ olength1.scm
@@ -1,14 +1,23 @@
 (load "./common/add1.scm")
 (load "./eternity.scm")
 
-(define olength0 ; here define is not used in the Little Schemer
+(define olength<=1 ; here define is not used in the Little Schemer
   (lambda (l)
     (cond
       ((null? l) 0)
-      (else (add1 (olength0 (cdr l))))))
+      (else
+       (add1
+        ((lambda (l)
+           (cond
+             ((null? l) 0)
+             (else (add1
+                    (eternity (cdr l))))))
+         (cdr l))))))
   )
 
-; > (olength0 `())
+; > (olength<=1 `())
 ; 0
-; > (olength0 `(hotdogs with mustard sauerkraut and pickles))
-; 6 ; this would not work without define
+; > (olength<=1 `(hotdogs))
+; 1
+; > (olength<=1 `(hotdogs with))
+; never stops
