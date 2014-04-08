891ab9e2d57a3d9c1aa3609ebb786d206028ba63
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-09 01:52:46 +0900

    modify length<=2 Chap09 p167 L1 (right)

--- olength2.scm
+++ olength2.scm
@@ -2,22 +2,15 @@
 (load "./eternity.scm")
 
 (define olength<=2 ; here define is not used in the Little Schemer
-  ((lambda (length)
+  ((lambda (mk-length)
+     (mk-length
+      (mk-length
+       (mk-length eternity))))
+   (lambda (length)
      (lambda (l)
        (cond
          ((null? l) 0)
-         (else (add1 (length (cdr l)))))))
-   ((lambda (length)
-      (lambda (l)
-        (cond
-          ((null? l) 0)
-          (else (add1 (length (cdr l)))))))
-    ((lambda (length)
-       (lambda (l)
-         (cond
-           ((null? l) 0)
-           (else (add1 (length (cdr l)))))))
-     eternity)))
+         (else (add1 (length (cdr l))))))))
   )
 
 ; > (olength<=2 `())
