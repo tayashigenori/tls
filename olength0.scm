7605731800501b8d5b5fef21c96b02558334b470
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-09 01:56:12 +0900

    modify length0 Chap09 p168 L3 (rigth)

--- olength0.scm
+++ olength0.scm
@@ -3,12 +3,12 @@
 (define olength0 ; here define is not used in the Little Schemer
   ((lambda (mk-length)
      (mk-length mk-length))
-   (lambda (length)
+   (lambda (mk-length)
      (lambda (l)
        (cond
          ((null? l) 0)
          (else (add1
-                (length (cdr l))))))))
+                (mk-length (cdr l))))))))
   )
 
 ; > (olength0 `())
