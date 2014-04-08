ccc2badbf20c919b0dca35853ca1ae2302391c6a
Shigenori Taya <shigenori.taya@gmail.com>
2014-04-09 02:08:23 +0900

    modify olength Chap09 p174 L4

--- olength.scm
+++ olength.scm
@@ -1,17 +1,17 @@
 (load "./common/add1.scm")
 
 (define olength
-  ((lambda (mk-length)
-     (mk-length mk-length))
-   (lambda (mk-length)
-     ((lambda (length)
-        (lambda (l)
-          (cond
-            ((null? l) 0)
-            (else
-             (add1 (length (cdr l)))))))
-      (lambda (x)
-        ((mk-length mk-length) x)))))
+  ((lambda (le)
+     ((lambda (mk-length)
+        (mk-length mk-length))
+      (lambda (mk-length)
+        (le (lambda (x)
+              ((mk-length mk-length) x))))))
+   (lambda (length)
+     (lambda (l)
+       (cond
+         ((null? l) 0)
+         (else (add1 (length (cdr l))))))))
   )
 
 ; > (olength `(hotdogs with mustard sauerkraut and pickles))
