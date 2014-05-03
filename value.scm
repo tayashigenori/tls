5cc1a9ae9937177e125aa0e5fac800ce19340baf
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-04 00:42:09 +0900

    add lookup-in-entry in value.scm Chap10 p178 L1

--- value.scm
+++ value.scm
@@ -5,6 +5,8 @@
 (load "./atom-to-function.scm")
 
 (load "./build.scm")
+(load "./first.scm")
+(load "./second.scm")
 
 (define value
   (lambda (nexp)
@@ -28,3 +30,24 @@
 
 (define new-entry build)
 
+(define lookup-in-entry
+  (lambda (name entry entry-f)
+    (lookup-in-entry-help name
+                          (first entry)
+                          (second entry)
+                          entry-f)))
+
+(define lookup-in-entry-help
+  (lambda (name names values entry-f)
+    (cond
+      ((null? names) (entry-f name))
+      ((eq? (car names) name)
+       (car values))
+      (else (lookup-in-entry-help name
+                                  (cdr names)
+                                  (cdr values)
+                                  entry-f)))))
+
+
+; > (lookup-in-entry `entree `((appetizer entree beverage) (food tastes good)) first) ; "first" not used
+; tastes
