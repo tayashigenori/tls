39c78fd2c9e598a4155d7758af6fc1093170ab97
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-04 00:45:33 +0900

    add lookup-in-table in value.scm Chap10 p179 L1

--- value.scm
+++ value.scm
@@ -54,3 +54,21 @@
 
 (define extend-table cons)
 
+(define lookup-in-table
+  (lambda (name table table-f)
+    (cond
+      ((null? table) (table-f name))
+      (else (lookup-in-entry name
+                             (car table)
+                             (lambda (name)
+                               (lookup-in-table name
+                                                (cdr table)
+                                                table-f)))))))
+
+
+; > (lookup-in-table `entree `(((entree dessert)
+;                               (spaghetti spumoni))
+;                              ((appetizer entree beverage)
+;                               (food tastes good)))
+;                    first) ; "first" not used
+; spaghetti
