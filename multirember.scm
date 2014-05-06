8aaf463233dd46d21517c3f6f72fc34aa0e7f1cf
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-06 23:28:52 +0900

    add mrpie Chap12 p21 L6
    temporal function only used for explanation

--- multirember.scm
+++ multirember.scm
@@ -1,18 +1,15 @@
-(define multirember
-  (lambda (a lat)
-    ((letrec
-         ((mr (lambda (lat)
-                (cond
-                  ((null? lat) (quote ()))
-                  ((eq? a (car lat))
-                   (mr (cdr lat)))
-                  (else
-                   (cons (car lat)
-                         (mr (cdr lat))))))))
-       mr)
-     lat)))
+; function defined for the purpose of explanation
+(define mrpie
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) (quote pie))
+       (mrpie (cdr lat)))
+      (else (cons (car lat)
+                  (mrpie (cdr lat)))))))
 
 ; > (multirember `cup `(coffee cup tea cup and hick cup))
 ; (coffee tea and hick)
 ; > (multirember `pie `(apple custard pie linzer pie torte))
 ; (apple custard linzer torte)
+
