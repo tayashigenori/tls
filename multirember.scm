8d31f1cddd435e16a8022354fdec6d09e00afdd2
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-06 23:45:14 +0900

    modify multirember Chap12 p25 L4

--- multirember.scm
+++ multirember.scm
@@ -1,16 +1,16 @@
-; see 2db4db77d688a09d7e507f69717f8c1174436727
-
 (define multirember
   (letrec
-      ((mr (lambda (a lat)
-             (cond
-               ((null? lat) (quote ()))
-               ((eq? (car lat) a)
-                (mr a (cdr lat)))
-               (else
-                (cons (car lat)
-                      (mr a (cdr lat))))))))
-    mr))
+      ((multirember
+        (lambda (a lat)
+          (cond
+            ((null? lat) (quote ()))
+            ((eq? (car lat) a)
+             (multirember a (cdr lat)))
+            (else
+             (cons (car lat)
+                   (multirember a
+                                (cdr lat))))))))
+    multirember))
 
 ; > (multirember `cup `(coffee cup tea cup and hick cup))
 ; (coffee tea and hick)
