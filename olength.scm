f93dfb9c604fce0fc8afd888d959305e93f0e8b0
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-06 23:18:33 +0900

    modify length (olength) Chap12 p17 L5 (right)

--- olength.scm
+++ olength.scm
@@ -1,16 +1,13 @@
 (load "./common/add1.scm")
+(load "./y.scm")
 
 (define olength
-  ((lambda (le)
-     ((lambda (f) (f f))
-      (lambda (f)
-        (le (lambda (x) ((f f) x))))))
-   (lambda (length)
-     (lambda (l)
-       (cond
-         ((null? l) 0)
-         (else
-          (add1 (olength (cdr l)))))))))
+  (Y (lambda (length)
+       (lambda (l)
+         (cond
+           ((null? l) 0)
+           (else
+            (add1 (olength (cdr l)))))))))
 
 ; > (olength `(hotdogs with mustard sauerkraut and pickles))
 ; 6
