bec8523e33d5bca12f4f81f946bb1000dfd041a5
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-07 00:12:24 +0900

    modify sum-of-prefixes Chap12 p34 L3

--- sum-of-prefixes.scm
+++ sum-of-prefixes.scm
@@ -2,22 +2,18 @@
 
 (define sum-of-prefixes
   (lambda (tup)
-    (sum-of-prefixes-b 0 tup)))
+    (letrec
+        ((S (lambda (sss tup)
+              (cond
+                ((null? tup) (quote ()))
+                (else
+                 (cons (o+ sss (car tup))
+                       (S (o+ sss (car tup))
+                          (cdr tup))))))))
+      (S 0 tup))))
 
 
 ; > (sum-of-prefixes `(2 1 9 17 0))
 ; (2 3 12 29 29)
 ; > (sum-of-prefixes `(1 1 1 1 1))
 ; (1 2 3 4 5)
-
-(define sum-of-prefixes-b
-  (lambda (sonssf tup) ; sum  of numbers seen so far
-    (cond
-      ((null? tup) (quote ()))
-      (else (cons (o+ sonssf (car tup))
-                  (sum-of-prefixes-b
-                   (o+ sonssf (car tup))
-                   (cdr tup)))))))
-
-; > (sum-of-prefixes-b 0 `(1 1 1))
-; (1 2 3)
