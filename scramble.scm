84e12e5f75b072e3dce0eda341dc03a42557ff2f
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-06 17:55:32 +0900

    add scramble Chap11 p14 L6

--- scramble.scm
+++ scramble.scm
@@ -10,3 +10,15 @@
              (scramble-b (cdr tup)
                          (cons (car tup) rev-pre)))))))
 
+
+(define scramble
+  (lambda (tup)
+    (scramble-b tup (quote ()))))
+
+
+; > (scramble `(1 1 1 3 4 2 1 1 9 2))
+; (1 1 1 1 1 4 1 1 1 9)
+; > (scramble `(1 2 3 4 5 6 7 8 9))
+; (1 1 1 1 1 1 1 1 1)
+; > (scramble `(1 2 3 1 2 3 4 1 8 2 10))
+; (1 1 1 1 1 1 1 1 2 8 2)
