9cf77bcb7e6db25a18c3f7e7e55dcceb7c0734cb
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-07 00:10:19 +0900

    modify two-in-a-row? Chap12 p33 L4

--- two-in-a-row.scm
+++ two-in-a-row.scm
@@ -1,9 +1,15 @@
 (define two-in-a-row?
   (lambda (lat)
-    (cond
-      ((null? lat) #f)
-      (else (two-in-a-row-b? (car lat)
-                             (cdr lat))))))
+    (letrec
+        ((W (lambda (a lat)
+              (cond
+                ((null? lat) #f)
+                (else (or (eq? (car lat) a)
+                          (W (car lat)
+                             (cdr lat))))))))
+      (cond
+        ((null? lat) #f)
+        (else (W (car lat) (cdr lat)))))))
 
 ; > (two-in-a-row? `(Italian sardines spaghetti parsley))
 ; #f
@@ -13,24 +19,3 @@
 ; #f
 ; > (two-in-a-row? `(b d e i i a g))
 ; #t
-
-(define is-first?
-  (lambda (a lat)
-    (cond
-      ((null? lat) #f)
-      (else (eq? (car lat) a)))))
-
-(define is-first-b?
-  (lambda (a lat)
-    (cond
-      ((null? lat) #f)
-      (else (or (eq? (car lat) a)
-                (two-in-a-row? lat))))))
-
-(define two-in-a-row-b?
-  (lambda (preceding lat)
-    (cond
-      ((null? lat) #f)
-      (else (or (eq? (car lat) preceding)
-                (two-in-a-row-b? (car lat)
-                                 (cdr lat)))))))
