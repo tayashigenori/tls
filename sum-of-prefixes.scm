e483ced3a08a57ebc26a0636025d8fd8defbdf6d
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-06 17:50:30 +0900

    add sum-of-prefixes-b Chap11 p10 L6

--- sum-of-prefixes.scm
+++ sum-of-prefixes.scm
@@ -1,3 +1,5 @@
+(load "o+.scm")
+
 (define sum-of-prefixes
   (lambda (tup)
     (cond
@@ -7,3 +9,15 @@
        ...))))
 
 ; doesn't work yet
+
+(define sum-of-prefixes-b
+  (lambda (sonssf tup) ; sum  of numbers seen so far
+    (cond
+      ((null? tup) (quote ()))
+      (else (cons (o+ sonssf (car tup))
+                  (sum-of-prefixes-b
+                   (o+ sonssf (car tup))
+                   (cdr tup)))))))
+
+; > (sum-of-prefixes-b 0 `(1 1 1))
+; (1 2 3)
