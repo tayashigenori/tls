caff3edd79f14f26af218c2d5c7c4c4ad7ae121b
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-06 23:30:15 +0900

    reverting multirember Chap12 p21 L7

--- multirember.scm
+++ multirember.scm
@@ -1,15 +1,18 @@
-; function defined for the purpose of explanation
-(define mrpie
-  (lambda (lat)
-    (cond
-      ((null? lat) (quote ()))
-      ((eq? (car lat) (quote pie))
-       (mrpie (cdr lat)))
-      (else (cons (car lat)
-                  (mrpie (cdr lat)))))))
+(define multirember
+  (lambda (a lat)
+    ((letrec
+         ((mr (lambda (lat)
+                (cond
+                  ((null? lat) (quote ()))
+                  ((eq? a (car lat))
+                   (mr (cdr lat)))
+                  (else
+                   (cons (car lat)
+                         (mr (cdr lat))))))))
+       mr)
+     lat)))
 
 ; > (multirember `cup `(coffee cup tea cup and hick cup))
 ; (coffee tea and hick)
 ; > (multirember `pie `(apple custard pie linzer pie torte))
 ; (apple custard linzer torte)
-
