3b7edd9a93cab60e2591bb735b928dca68960134
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-11 23:07:54 +0900

    add rember-beyond-first Chap13 p54 L2

--- rember.scm
+++ rember.scm
@@ -11,3 +11,41 @@
 
 ; > (rember `sausage `(pizza with sausage and bacon))
 ; (pizza with and bacon)
+
+(define rember-beyond-first
+  (lambda (a lat)
+    (letrec
+        ((R (lambda (lat)
+              (cond
+                ((null? lat) (quote ()))
+                ((eq? (car lat) a)
+                 (quote ()))
+                (else (cons (car lat)
+                            (R (cdr lat))))))))
+      (R lat))))
+
+; > (rember-beyond-first `roots `(noodles
+;                                 spaghetti spatzle bean-thread
+;                                 roots
+;                                 potatoes yam
+;                                 others
+;                                 rice))
+; (noodles spaghetti spatzle bean-thread)
+; > (rember-beyond-first (quote others) `(noodles
+;                                         spaghetti spatzle bean-thread
+;                                         roots
+;                                         potatoes yam
+;                                         others
+;                                         rice))
+; (noodles spaghetti spatzle bean-thread roots potatoes yam)
+; > (rember-beyond-first (quote desserts) `(cookies
+;                                           chocolate mints
+;                                           caramel delight ginger snaps
+;                                           desserts
+;                                           chocolate mousse
+;                                           vanilla ice cream
+;                                           German chocolate cake
+;                                           more desserts
+;                                           gingerbreadman chocolate
+;                                           chip brownies))
+; (cookies chocolate mints caramel delight ginger snaps)
